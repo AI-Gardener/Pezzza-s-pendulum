1c7174c879a2eb1730f0e4ee5b19632f084d7b62
AI-Gardener <anaigardener@gmail.com>
2024-05-30 12:50:31 +0200

    Working multi-model loading Add the SVG, Blend, OBJ and MTL files of  the assets Working animation rendering Change State impl to DeltaT

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 // //! Run in release mode for better performance.
 // //! This is meant to be highly optimized, this is more exploration.
 
-use std::f32::consts::PI;
+use std::{collections::HashMap, f32::consts::PI, fs::File, io::BufReader, path::Path};
 use ai_template::*;
 use fastrand::f32;
 
@@ -25,7 +25,7 @@ impl Reinforcement for State {
     }
 
     fn num_generations() -> usize {
-        100
+        1000
     }
 
     fn ticks_per_evaluation() -> usize {
@@ -74,30 +74,32 @@ impl Reinforcement for State {
         self.cart_x_speed = dac.nodes[4].val;
     }
 
-    fn update_physics(&mut self) {
-        let new_cart_x = (self.cart_x + self.cart_x_speed * Self::tick_duration()).max(-3.0).min(3.0);
-        self.cart_x_speed = (new_cart_x - self.cart_x) / Self::tick_duration();
+    fn update_physics(&mut self, delta_t: f32) {
+        let new_cart_x = (self.cart_x + self.cart_x_speed * delta_t).max(-3.0).min(3.0);
+        self.cart_x_speed = (new_cart_x - self.cart_x) / delta_t;
         self.cart_x = new_cart_x;
 
         let new_pendulum_angle = self.pendulum_angle +
             ( self.pendulum_ang_vel
             + self.pendulum_angle.sin() * self.cart_x_speed * 0.1
             - self.pendulum_angle.cos() * 0.7
-            ) * Self::tick_duration();
-        self.pendulum_ang_vel = (new_pendulum_angle - self.pendulum_angle) / Self::tick_duration();
+            ) * delta_t;
+        self.pendulum_ang_vel = (new_pendulum_angle - self.pendulum_angle) / delta_t;
         self.pendulum_angle = new_pendulum_angle;
     }
 
-    fn update_score(&mut self, score: &mut f32) {
+    fn update_score(&mut self, score: &mut f32, delta_t: f32) {
+        let mut result: f32 = 0.0;
         if self.pendulum_angle.sin() > 0.9 {
-            *score += 1.0;
+            result += 1.0;
             if self.cart_x.abs() < 0.1 {
-                *score += 1.0;
+                result += 1.0;
             }
             if self.cart_x_speed.abs() < 0.1 {
-                *score += 0.2;
+                result += 0.2;
             }
         }
+        *score += result * delta_t;
     }
 
     fn mutated(dac: &DAC) -> DAC {
@@ -118,41 +120,18 @@ impl Reinforcement for State {
     }
 
     fn draw_vertices() -> (Vec<InputVertex>, Option<Vec<u32>>) {
-        use tobj;
-
-        let cart = tobj::load_obj("./scene/cart.obj", &tobj::GPU_LOAD_OPTIONS);
-        assert!(cart.is_ok());
-        
-        let (cart_models, cart_materials) = cart.expect("Failed to load OBJ file");
-        
-        // Materials might report a separate loading error if the MTL file wasn't found.
-        // If you don't need the materials, you can generate a default here and use that
-        // instead.
-        let cart_materials = cart_materials.expect("Failed to load MTL file");
-        
-        println!("# of models: {}", cart_models.len());
-        println!("# of materials: {}", cart_materials.len());
-        
-        let cart_indices = cart_models.iter()
-            .flat_map(|model| model.mesh.indices.clone())
-            .collect::<Vec<u32>>();
-        let cart_vertices = cart_models.iter()
-            .flat_map(|model| model.mesh.positions.chunks_exact(3).collect::<Vec<&[f32]>>())
-            .map(|chunk| InputVertex::new(
-                [f32(), 1.0, f32(), 1.0],
-                [chunk[0] * 5.0, chunk[2] * 5.0, 0.0],
-                0
-            ))
-            .collect::<Vec<InputVertex>>();
+        let (vertices, indices) = load_model("./scene/shapes.obj");
         (
-            cart_vertices,
-            Some(cart_indices)
+            vertices,
+            Some(indices)
         )
     }
 
     fn draw_transformations(&self, matrices: &mut [Mat4; 32]) {
-        matrices[0] = Mat4::translate(0.5, 0.0, 0.0);
-        matrices[1] = Mat4::translate(0.0, 0.5, 0.0);
+        println!("Cart x: {}", self.cart_x);
+        matrices[0] = Mat4::translate(self.cart_x / 4.0, 0.0, 0.0);
+        matrices[1] = Mat4::rotate_z(self.pendulum_angle);
+        matrices[1].mult(Mat4::translate(self.cart_x / 4.0, 0.0, 0.0));
     }
     
     fn draw_view_present_mode() -> PresentMode {
@@ -168,6 +147,84 @@ fn main() {
     
     println!("Total: {:?}", start.elapsed()); // Longer esp. with printing operations.
 
-    let mut vk: Vk<State> = Vk::init();
-    vk.view();
+    let mut vk: Vk = Vk::init::<State>();
+    vk.view_agent(ai.best_agent());
+}
+
+// Code from a vulkanalia tutorial: https://github.com/KyleMayes/vulkanalia/blob/master/tutorial/src/27_model_loading.rs
+fn load_model<P: AsRef<Path>>(path: P) -> (Vec<InputVertex>, Vec<u32>) {
+    // Model
+
+    let mut reader = BufReader::new(File::open(path).unwrap());
+
+    let (models, _) = tobj::load_obj_buf(
+        &mut reader,
+        &tobj::LoadOptions {
+            single_index: false,
+            triangulate: true,
+            ignore_points: true,
+            ignore_lines: true,
+        },
+        |_| Ok(Default::default()),
+    ).unwrap();
+
+    // Vertices / Indices
+
+    let mut unique_vertices = HashMap::new();
+
+    let (mut vertices, mut indices): (Vec<InputVertex>, Vec<u32>) = (Vec::new(), Vec::new());
+
+    let mut z: f32 = -0.1;
+    for model in &models {
+        z+=0.1;
+        for index in &model.mesh.indices {
+            let pos_offset = (3 * index) as usize;
+
+            let vertex: InputVertex = if -model.mesh.positions[pos_offset + 2] < 0.1 {
+                InputVertex::new(
+                    [(f32() + 4.0) / 8.0, 0.0, (f32() + 4.0) / 8.0, 1.0],
+                    [
+                        (model.mesh.positions[pos_offset]) / 2.0,
+                        (- model.mesh.positions[pos_offset + 2]) / 2.0,
+                        z,
+                    ],
+                    0,
+                )
+                    
+            } else if -model.mesh.positions[pos_offset + 2] < 0.3 {
+                InputVertex::new(
+                    [0.0, (f32() + 4.0) / 8.0, 0.0, 1.0],
+                    [
+                        (model.mesh.positions[pos_offset]) / 2.0,
+                        (- model.mesh.positions[pos_offset + 2] - 0.2) / 2.0,
+                        z,
+                    ],
+                    1,
+                )
+                    
+            } else {
+                InputVertex::new(
+                    [1.0, 1.0, 1.0, 1.0],
+                    [
+                        (model.mesh.positions[pos_offset]) / 2.0,
+                        (- model.mesh.positions[pos_offset + 2]) / 2.0,
+                        z,
+                    ],
+                    2,
+                )
+                    
+            };
+
+            if let Some(index) = unique_vertices.get(&vertex) {
+                indices.push(*index as u32);
+            } else {
+                let index = vertices.len();
+                unique_vertices.insert(vertex, index);
+                vertices.push(vertex);
+                indices.push(index as u32);
+            }
+        }
+    }
+
+    (vertices, indices)
 }
